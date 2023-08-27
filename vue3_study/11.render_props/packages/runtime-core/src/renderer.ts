74e9894fe5debf58133950ee73bdc87c9d1848db
guojw <2315162186@qq.com>
2023-08-27 17:44:11 +0800

    render_props

---
+++ vue3_study/11.render_props/packages/runtime-core/src/renderer.ts
@@ -0,0 +1,77 @@
+import { ShapeFlags } from 'packages/shared/src/shapeFlags'
+import { Text, Fragment } from './vnode'
+
+export interface RendererOptions {
+  setElementText(node: Element, text: string): void
+  insert(el, parent: Element, anchor?): void
+  createElement(type: string): Element
+  patchProp(el: Element, key: string, prevValue: any, nextValue: any): void
+}
+
+export function createRenderer(options?: RendererOptions) {
+  const {
+    insert: hostInsert,
+    createElement: hostCreateElement,
+    setElementText: hostSetElementText,
+    patchProp: hostPatchProp
+  } = options!
+
+  return {
+    render
+  }
+  function render(vnode, container) {
+    if (vnode === null) {
+      // TODO 卸载
+    } else {
+      // 打补丁
+      patch(container._vnode, vnode, container)
+    }
+    // 更新旧节点
+    container._vnode = vnode
+  }
+  function patch(oldVNode, newVNode, container, anchor = null) {
+    if (oldVNode === newVNode) return
+    const { type, shapeFlag } = newVNode
+    switch (type) {
+      case Text:
+        // TODO
+        break
+      case Fragment:
+        // TODO
+        break
+      default:
+        if (shapeFlag & ShapeFlags.ELEMENT) {
+          // TODO
+          processElement(oldVNode, newVNode, container, anchor)
+        } else if (shapeFlag & shapeFlag.COMPONENT) {
+          // TODO
+        }
+    }
+  }
+  function processElement(oldVNode, newVNode, container, anchor) {
+    if (oldVNode == null) {
+      mountElement(newVNode, container, anchor)
+    } else {
+      // TODO卸载
+    }
+  }
+  function mountElement(vnode, container, anchor) {
+    const { type, shapeFlag, props } = vnode
+    // 1. 创建元素
+    const el = (vnode.el = hostCreateElement(type))
+    if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+      // 2. 设置文本
+      hostSetElementText(el, vnode.children)
+    } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+      // todo
+    }
+    // 3. 设置props
+    if (props) {
+      for (const key in props) {
+        hostPatchProp(el, key, null, props[key])
+      }
+    }
+    // 4. 插入
+    hostInsert(el, container, anchor)
+  }
+}
--- vue3_study/11.render_props/packages/runtime-dom/src/index.ts
+++ vue3_study/11.render_props/packages/runtime-dom/src/index.ts
@@ -1,8 +1,9 @@
 import { createRenderer } from '@vue/runtime-core'
 import { nodeOps } from './nodeOps'
+import { patchProp } from './patchProp'
 
 let renderer
-const rendererOptions = Object.assign({}, nodeOps)
+const rendererOptions = Object.assign({ patchProp }, nodeOps)
 // 这里是一种设计模式 叫**依赖注入**，通过参数的形式注入依赖
 export function render(...args) {
   // 缓存renderer实例
