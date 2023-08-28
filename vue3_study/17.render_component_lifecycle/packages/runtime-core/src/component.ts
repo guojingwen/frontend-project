2495e88bb1995f5e16e0baaba541ce625ad82ef7
guojw <2315162186@qq.com>
2023-08-28 17:19:19 +0800

    no message

--- vue3_study/17.render_component_lifecycle/packages/runtime-core/src/component.ts
+++ vue3_study/17.render_component_lifecycle/packages/runtime-core/src/component.ts
@@ -3,10 +3,17 @@ import { ReactiveEffect, reactive } from '@vue/reactivity'
 
 let uid = 0
 export function createComponentInstance(vnode): ComponentInstance {
+  const type = vnode.type
+  const {
+    beforeCreate = null,
+    created = null,
+    beforeMount = null,
+    mounted = null
+  } = type
   const instance = {
     uid: uid++,
     vnode,
-    type: vnode.type,
+    type,
     /**
      * subTree  render函数的返回值
      * 对应要渲染的DOM节点
@@ -16,7 +23,12 @@ export function createComponentInstance(vnode): ComponentInstance {
     update: null, // update 函数，触发 effect.run
     render: null, // 组件内的 render 函数
     isMounted: false,
-    data: null
+    data: null,
+    // 生命周期相关
+    beforeCreate,
+    created,
+    beforeMount,
+    mounted
   }
   return instance
 }
@@ -31,13 +43,31 @@ export interface ComponentInstance {
   render: null | Function
   isMounted: boolean
   data: any
+  // 生命周期相关
+  beforeCreate: null | Function // beforeCreate
+  created: null | Function // created
+  beforeMount: null | Function // beforeMount
+  mounted: null | Function // mounted
 }
 
 export function setupComponent(instance: ComponentInstance) {
   if (!instance.render) {
     instance.render = instance.type.render
   }
+
+  const { beforeCreate, created } = instance
+  if (beforeCreate) {
+    callHook(beforeCreate, null)
+  }
+
   applyOptions(instance)
+
+  if (created) {
+    callHook(created, instance.data)
+  }
+}
+export function callHook(hook: Function, proxy) {
+  hook.bind(proxy)()
 }
 
 function applyOptions(instance: ComponentInstance) {
