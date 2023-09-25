31c995c7262590719b355c0f3aad8815e138224f
guojw <2315162186@qq.com>
2023-09-25 20:27:53 +0800

    init

--- react_study/10.principle/step4/my-react-before.js
+++ react_study/10.principle/step4/my-react-before.js
@@ -160,45 +160,9 @@ function performUnitOfWork(fiber) {
   }
 }
 
-let wipFiber = null;
-let hookIndex = null;
 function updateFunctionComponent(fiber) {
-  wipFiber = fiber;
-  hookIndex = 0;
-  wipFiber.hooks = [];
   const children = [fiber.type(fiber.props)];
   reconcileChildren(fiber, children);
-}
-
-function useState(initial) {
-  const oldHook =
-    wipFiber.alternate &&
-    wipFiber.alternate.hooks &&
-    wipFiber.alternate.hooks[hookIndex];
-  const hook = {
-    state: oldHook ? oldHook.state : initial,
-    queue: [],
-  };
-
-  const actions = oldHook ? oldHook.queue : [];
-  actions.forEach((action) => {
-    hook.state = action(hook.state);
-  });
-
-  const setState = (action) => {
-    hook.queue.push(action);
-    wipRoot = {
-      dom: currentRoot.dom,
-      props: currentRoot.props,
-      alternate: currentRoot,
-    };
-    nextUnitOfWork = wipRoot;
-    deletions = [];
-  };
-
-  wipFiber.hooks.push(hook);
-  hookIndex++;
-  return [hook.state, setState];
 }
 
 function updateHostComponent(fiber) {
@@ -262,7 +226,6 @@ function reconcileChildren(wipFiber, elements) {
 const Didact = {
   createElement,
   render,
-  useState,
 };
 
 export default Didact;
